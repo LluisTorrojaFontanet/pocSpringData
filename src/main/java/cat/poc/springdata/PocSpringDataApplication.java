c1ea657121d6ef51d569c09beb50159398f78570
c127usu <ltorroja@cass.ad>
2023-01-27 17:05:52 +0100

    Initial commit

---
+++ src/main/java/cat/poc/springdata/PocSpringDataApplication.java
@@ -0,0 +1,13 @@
+package cat.poc.springdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocSpringDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocSpringDataApplication.class, args);
+	}
+
+}
